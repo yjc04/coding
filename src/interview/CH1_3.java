a4bdceaa32f0cde62966455ab436f84c58bcd5e3
yongjin <ycho1@andrew.cmu.edu>
2014-12-21 17:00:08 +0900

    edited

--- src/interview/CH1_3.java
+++ src/interview/CH1_3.java
@@ -3,22 +3,40 @@ package interview;
 import java.util.HashMap;
 import java.util.Map;
 
-public class CH1_3 {
-	public boolean isPerm(String s1, String s2){
-		Map map = new HashMap<String, Integer>();
-		
-		String big = s1.length() >= s2.length() ? s1 : s2;
-		String small = big.equals(s1) ? s2 : s1;
-		System.out.println(" big = " + big + "  small = " + small);
-		
-		for(int i = 0; i < small.length(); i++){
-			if(map.containsKey(small.charAt(i))){
-				map.put(small.charAt(i), (Integer)map.get(small.charAt(i)) + 1);
-			}else{
-				map.put(small.charAt(i), new Integer(1));
+
+//finds out if one of the string is the permutation of another string 
+public class CH1_3 {	
+	public static boolean isPerm(String s1, String s2){
+		if(s1.length() != s2.length()){
+			return false;
+		}else{
+			char[] a1 = s1.toCharArray();
+			char[] a2 = s2.toCharArray();
+			int[] a1Char = new int[256];
+			int[] a2Char = new int[256];
+			
+			//update the char frequency for two strings
+			for(char c : a1){
+				a1Char[c]++;
 			}
+			for(char c : a2){
+				a2Char[c]++;
+			}
+			
+			for(char c : a1){
+				if(a1Char[c] != a2Char[c]){
+					return false;
+				}
+			}
+			
+			return true;
 		}
-		return true;
-		//
+	}
+	
+	public static void main(String[] args){
+		String t1 = "abcd";
+		String t2 = "acdb";
+		
+		System.out.println("test result = " + isPerm(t1,t2));
 	}
 }
